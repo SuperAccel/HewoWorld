fd07c7ebc9c1a606c5052cf6dd34217a0fbe46ba
Anthony Tran <SuperAccel@yahoo.com>
2022-12-21 17:54:55 -0600

    Fixed2

--- HewoWorld/Program.cs
+++ HewoWorld/Program.cs
@@ -2,11 +2,3 @@
 Console.WriteLine("Hewo World!");
 Console.WriteLine("Testing 123ABC.");
 Console.WriteLine("TestingAgain.");
-sdjafkasdhfjksdafds
-    FAIFASLDASDASds
-    ad
-    asd
-    asd
-    asgdfh
-    gcfhfg
-    dhfd
