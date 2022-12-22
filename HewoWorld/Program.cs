b0e24dba9b1fe59beec23aaf080bd128475ea21c
Anthony Tran <SuperAccel@yahoo.com>
2022-12-21 19:34:01 -0600

    Again!

--- HewoWorld/Program.cs
+++ HewoWorld/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-abcasdsd
+Console.WriteLine("Hewo World!");
+Console.WriteLine("Testing 123ABC.");
+Console.WriteLine("TestingAgain.");
+Console.WriteLine("Did it work2?");
